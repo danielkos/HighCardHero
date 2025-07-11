8e6f8d458fa0174e2dd5e6b4659b5e8dfe20a0c1
Daniel Kosinski <daniel.kosinski1991@gmail.com>
2025-07-11 23:30:30 +0200

    Added example of an evaluation of two hands

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,51 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import PokerCard.Card;
+import PokerCard.CardValue;
+import PokerCard.CardSuit;
+import PokerHand.Hand;
+import PokerHand.RankedHand;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.util.List;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello and welcome!");
+
+        Hand first = new Hand(List.of(
+            new Card(CardSuit.C, CardValue.TWO),
+            new Card(CardSuit.H, CardValue.TWO),
+            new Card(CardSuit.D, CardValue.FOUR),
+            new Card(CardSuit.S, CardValue.FIVE),
+            new Card(CardSuit.C, CardValue.SEVEN)
+        ));
+
+        Hand second = new Hand(List.of(
+            new Card(CardSuit.C, CardValue.THREE),
+            new Card(CardSuit.D, CardValue.THREE),
+            new Card(CardSuit.D, CardValue.THREE),
+            new Card(CardSuit.S, CardValue.SIX),
+            new Card(CardSuit.H, CardValue.EIGHT)
+        ));
+
+        // Rank both hands
+        final RankedHand firstRankedHand = HandEvaluator.evaluate(first);
+        final RankedHand secondRankedHand = HandEvaluator.evaluate(second);
+        // Compare ranks
+        final int result = firstRankedHand.compareTo(secondRankedHand);
+
+        // Display result
+        System.out.println("Competing hands: ");
+        System.out.println("1: " + first);
+        System.out.println("2: " + second);
+
+        if (result > 0) {
+            System.out.println("First hand wins: " + firstRankedHand + "!");
+        } else if (result < 0) {
+            System.out.println("Second hand wins: " + secondRankedHand + "!");
+        } else  {
+            System.out.println("It's a tie");
         }
     }
 }
